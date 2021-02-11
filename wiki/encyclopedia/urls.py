0bbb5a6fb2443744291d58c5e7488ca2988e7ceb
lyu20 <32178113+lyu20@users.noreply.github.com>
2021-02-11 12:05:45 -0500

    working on encyclopedia
    next step: work on edit page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/search/", views.search, name="search"),
+    path("newpage", views.new_page, name="new_page"),
+    path("add", views.add, name="add")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
+from django import forms
+from django.utils.safestring import mark_safe
+
 from . import util
 
 
@@ -17,3 +22,46 @@ def entry(request, title):
             "title": title.capitalize(),
             "entry": util.get_entry(title)
         })
+
+def search(request):
+    form = request.GET
+    search = form['q'].lower()
+    print(search)
+    print(util.list_entries())
+    if search in (title.lower() for title in util.list_entries()):
+        return entry(request, search)
+    else:
+        return search_results(request, search)
+
+def search_results(request, search):
+    get_items = []
+    print(type(util.list_entries()[1]))
+    print(type(search))
+    for entry in util.list_entries():
+        if search in entry.lower():
+            get_items.append(entry)
+    return render(request, "encyclopedia/search_results.html", {
+        "entries": get_items
+    })
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label=mark_safe("Title"))
+    content = forms.CharField(widget=forms.Textarea, label=mark_safe("Content"))
+
+def new_page(request, title=None):
+    return render(request, "encyclopedia/new_page.html", {
+        "form": NewPageForm(),
+        "title": title
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+    if form.is_valid():
+        title = form.cleaned_data["title"]
+        for page in util.list_entries():
+            if page.lower() == title.lower():
+                return new_page(request, title)
+        content = form.cleaned_data["content"]
+        util.save_entry(title, content)
+        return entry(request, title)
